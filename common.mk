3T-common/android_device_oneplus_sm8250-common

sm8250-common: Don't use BSG framework for gpt-utils
Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

common.mk
@@ -95,6 +95,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+$(call soong_config_set,OPLUS_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \

RisingOS-Revived-devices/android_device_motorola_sm8250-common

common.mk
@@ -190,6 +190,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \